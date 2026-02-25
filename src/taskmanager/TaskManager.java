17c099a2a6e0e6743c8f3fb3d7a949da1e4d8f20
Jan Thielert <janthielertjan@gmail.com>
2026-02-25 15:20:36 +0100

    Den Taskmanager hinzugefügt

---
+++ src/taskmanager/TaskManager.java
@@ -0,0 +1,25 @@
+package taskmanager;
+
+import java.util.ArrayList;
+
+public class TaskManager {
+
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    public void addTask(String title) {
+        Task newTask = new Task(title);
+        tasks.add(newTask);
+    }
+
+    public void showTasks() {
+        if(tasks.isEmpty()){
+            System.out.println("No tasks available.");
+            return;
+        }
+
+        for(int i = 0; i < tasks.size(); i++){
+            Task t = tasks.get(i);
+            System.out.println((i+1) + ". " + t.getTitle() + (t.isDone() ? " [Done]" : ""));
+        }
+    }
+}
