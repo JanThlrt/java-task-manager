efd1ea39e8fcd43b50088b96d02ef8c51cfd040d
Jan Thielert <janthielertjan@gmail.com>
2026-02-25 15:30:33 +0100

    Main Klasse erweitert mit ausführbaren tasks funktionen

--- src/taskmanager/Main.java
+++ src/taskmanager/Main.java
@@ -1,7 +1,30 @@
 package taskmanager;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Task Manager started!");
+
+        Scanner scanner = new Scanner(System.in);
+        TaskManager manager = new TaskManager();
+
+        while(true){
+
+            System.out.println("\n1 - Add task");
+            System.out.println("2 - Show tasks");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if(choice == 1){
+                System.out.print("Enter task title: ");
+                String title = scanner.nextLine();
+                manager.addTask(title);
+            }
+            else if(choice == 2){
+                manager.showTasks();
+            }
+        }
     }
 }
